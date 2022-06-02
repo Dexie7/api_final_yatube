e6d7d5cc7be80332cc3cec359ec3dd2601dbe22e
Евгений Пермяков <evgenyu.permyakov@yandex.ru>
2022-06-02 23:41:24 +0500

    My 1 commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,52 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.exceptions import ValidationError
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+    pub_date = serializers.DateTimeField(read_only=True,
+                                         format='%Y-%m-%dT%H:%M:%SZ')
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+        read_only_fields = ('post', )
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True,
+                                        slug_field='username')
+    following = serializers.SlugRelatedField(slug_field='username',
+                                             queryset=User.objects)
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following', )
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        following = validated_data['following']
+        if user == following:
+            raise ValidationError(detail='Нельзя подписаться на самого себя')
+        if len(Follow.objects.filter(user=user, following=following)) != 0:
+            raise ValidationError(code=400, detail='Вы уже подписаны')
+        return Follow.objects.create(user=user, following=following)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,49 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, mixins, permissions, filters
+from posts.models import Follow, Group, Post
+
+from .permissions import AuthorOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer,
+                          GroupSerializer, PostSerializer)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly, )
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        serializer.save(author=self.request.user,
+                        post=get_object_or_404(Post, id=post_id))
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        new_queryset = get_object_or_404(Post, id=post_id).comments
+        return new_queryset
+
+
+class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('=following__username',)
+
+    def get_queryset(self):
+        user = self.request.user
+        new_queryset = Follow.objects.filter(user=user)
+        return new_queryset
